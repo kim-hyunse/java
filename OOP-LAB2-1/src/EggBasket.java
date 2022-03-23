cf0d6264fd0e670b054ae29197e913f8881909e2
kim-hyunse <99864704+kim-hyunse@users.noreply.github.com>
2022-03-23 09:36:21 +0900

    Add files via upload

---
+++ OOP-LAB2-1/src/EggBasket.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class EggBasket {
+
+	public static void main(String[] args) {
+		int numberOFBaskets, eggsPerBasket, totalEggs;
+		
+		numberOFBaskets=10;
+		eggsPerBasket=6;
+		
+		
+		totalEggs=numberOFBaskets*eggsPerBasket;
+		
+		System.out.println("If you have");
+		System.out.println(eggsPerBasket+" eggs Per Basket and");
+		System.out.println(numberOFBaskets+ " Baskets, then");
+		System.out.println("the total number of Eggs is " +totalEggs);
+		
+		
+		
+		
+
+	}
+
+}
