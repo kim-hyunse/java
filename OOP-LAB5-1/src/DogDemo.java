c11d409c2bb8bf1e391b4465c15c16caf8ad3520
kim-hyunse <99864704+kim-hyunse@users.noreply.github.com>
2022-04-11 20:12:27 +0900

    Add files via upload

---
+++ OOP-LAB5-1/src/DogDemo.java
@@ -0,0 +1,14 @@
+
+public class DogDemo {
+
+	public static void main(String[] args) {
+		
+		Dog balto=new Dog();
+		balto.name="balto";
+		balto.breed="siberian husky";
+		balto.age=8;
+		balto.writeOutput();
+		
+		
+	}
+}
