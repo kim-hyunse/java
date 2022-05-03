722633fe0fb23af7fb9cab266865bdd70aa534bf
kim-hyunse <wjwjwj2002@gachon.ac.kr>
2022-05-03 17:00:58 +0900

    oop exercise 8-2 rectangle abstract

---
+++ OOP-Ex-8-2-Rectangle/src/Circle.java
@@ -0,0 +1,20 @@
+public class Circle implements Measurable
+{
+private double myRadius;
+public Circle(double radius)
+{
+myRadius = radius;
+}
+public double getPerimeter ()
+{
+return 2 * Math.PI * myRadius;
+}
+public double getCircumference ()
+{
+return getPerimeter ();
+}
+public double getArea ()
+{
+return Math.PI * myRadius * myRadius;
+}
+}
---
+++ OOP-Ex-8-2-Rectangle/src/Measurable.java
@@ -0,0 +1,8 @@
+public interface Measurable
+{
+public static final int INCHES_PER_FOOT = 12;
+/** Returns the perimeter. */
+public double getPerimeter();
+/** Returns the area. */
+public double getArea();
+}
