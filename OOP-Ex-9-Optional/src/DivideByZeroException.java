9366cee408503cba69b53eeaa47edbf238a67b0c
kim-hyunse <wjwjwj2002@gachon.ac.kr>
2022-06-29 01:41:06 +0900

    make your own exception: dividebyzero

---
+++ OOP-Ex-9-Optional/src/DivideByZeroException.java
@@ -0,0 +1,13 @@
+
+public class DivideByZeroException extends Exception {
+
+	public DivideByZeroException() {
+		super("Dividing by zero");
+	}
+
+	public DivideByZeroException(String message) {
+		super(message);
+
+	}
+
+}
